075f95354112377ade06268883caa3500519667d
Thulana Thidaswin <Thidaswin@gmail.com>
2022-03-30 23:28:01 +0530

    Made the ViewPrice() function

--- The program/src/PizzaYard_Pvt_Ltd.java
+++ The program/src/PizzaYard_Pvt_Ltd.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PizzaYard_Pvt_Ltd {
-    private static void Menu(){
+    private static void Menu() {
         System.out.println("--------------------------------");
         System.out.println("Our Menu");
         System.out.println("1) Pizza");
@@ -9,7 +9,8 @@ public class PizzaYard_Pvt_Ltd {
         System.out.println("--------------------------------");
         System.out.print("Please enter your option   ");
     }
-    private static void MeatMenu(){
+
+    private static void MeatMenu() {
         System.out.println("--------------------------------");
         System.out.println("Select your meat Type please ");
         System.out.println("1) Chicken ");
@@ -19,7 +20,8 @@ public class PizzaYard_Pvt_Ltd {
         System.out.print("Please enter your option   ");
 
     }
-    private static void AdditonalMenu(){
+
+    private static void AdditonalMenu() {
         System.out.println("--------------------------------");
         System.out.println("Select 4 additionals");
         System.out.println("1) Cabbage ");
@@ -33,7 +35,8 @@ public class PizzaYard_Pvt_Ltd {
         System.out.print("Please enter your option   ");
 
     }
-    private  static void PizzaTypeMenu(){
+
+    private static void PizzaTypeMenu() {
         System.out.println("--------------------------------");
         System.out.println("Select your Pizza type");
         System.out.println("1) Normal Pan pizza ");
@@ -41,25 +44,27 @@ public class PizzaYard_Pvt_Ltd {
         System.out.println("3) Premium Pan Pizza");
         System.out.print("Please enter your option   ");
     }
-    public static void main(String[] args){
-        Scanner userInput=new Scanner(System.in);
-        Integer menuInput=0;
-        Integer breadtype=0;
-        Integer meatType=0;
-        Integer optionalItems=0;
-        Integer userItem=0;
-        Integer[] items = {0,0,0, 0,0,0};
-        Integer type=0;
-        Boolean valid=false;
-        Integer counter=0;
-        Double addtionalPrice=0.00;
-        Pizza NormalPizza=new Pizza(breadtype,meatType);
-        HealthyPizza HPizza=new HealthyPizza(breadtype,meatType);
-        Preminum_Pizza PrePizza=new Preminum_Pizza(breadtype,meatType);
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        Integer menuInput = 0;
+        Integer breadtype = 0;
+        Integer meatType = 0;
+        Integer optionalItems = 0;
+        Integer userItem = 0;
+        Integer[] items = {0, 0, 0, 0, 0, 0};
+        Integer type = 0;
+        Boolean valid = false;
+        Integer counter = 0;
+        Double addtionalPrice = 0.00;
+
+        Pizza NormalPizza = new Pizza(breadtype, meatType, addtionalPrice);
+        HealthyPizza HPizza = new HealthyPizza(breadtype, meatType, addtionalPrice);
+        Preminum_Pizza PrePizza = new Preminum_Pizza(breadtype, meatType, addtionalPrice);
 
         System.out.println("Welcome to PizzaYard Private Limited");
         PizzaTypeMenu();
-        while (valid==false) {
+        while (valid == false) {
             try {
                 type = Integer.parseInt(userInput.nextLine());
                 if (type == 1 | type == 2 | type == 3) {
@@ -74,62 +79,68 @@ public class PizzaYard_Pvt_Ltd {
         }
 
         MeatMenu();
-        try{
-            meatType=Integer.parseInt(userInput.nextLine());
+        try {
+            meatType = Integer.parseInt(userInput.nextLine());
         } catch (NumberFormatException e) {
             System.out.println("Please enter a number please");
         }
-        if(type==1){
+        if (type == 1) {
             NormalPizza.setMtype(meatType);
         }
-        if(type==2){
-           HPizza.setBtype(2);
-           HPizza.setMtype(meatType);
+        if (type == 2) {
+            HPizza.setBtype(1);
+            HPizza.setMtype(meatType);
         }
-        if(type==1){
-           PrePizza.setMtype(meatType);
+        if (type == 1) {
+            PrePizza.setMtype(meatType);
         }
         AdditonalMenu();
-        while (optionalItems<4){
-            try{
-                userItem=Integer.parseInt(userInput.nextLine());
-                items[optionalItems]=userItem;
+        while (optionalItems < 4) {
+            try {
+                userItem = Integer.parseInt(userInput.nextLine());
+                items[optionalItems] = userItem;
                 optionalItems++;
             } catch (NumberFormatException e) {
                 System.out.println("Please enter a number please");
             }
         }
-        while (counter<6) {
-            if (items[counter]==1) {
-                addtionalPrice=addtionalPrice+2.89;
+        while (counter < 6) {
+            if (items[counter] == 1) {
+                addtionalPrice = addtionalPrice + 2.89;
             }
-            if (items[counter]==2) {
-                addtionalPrice=addtionalPrice+3.5;
+            if (items[counter] == 2) {
+                addtionalPrice = addtionalPrice + 3.5;
             }
-            if (items[counter]==3) {
-                addtionalPrice=addtionalPrice+4.9;
+            if (items[counter] == 3) {
+                addtionalPrice = addtionalPrice + 4.9;
             }
-            if (items[counter]==4) {
-                addtionalPrice=addtionalPrice+1.69;
+            if (items[counter] == 4) {
+                addtionalPrice = addtionalPrice + 1.69;
             }
-            if (items[counter]==5) {
-                addtionalPrice=addtionalPrice+9.25;
+            if (items[counter] == 5) {
+                addtionalPrice = addtionalPrice + 9.25;
             }
-            if (items[counter]==6) {
-                addtionalPrice=addtionalPrice+2.22;
+            if (items[counter] == 6) {
+                addtionalPrice = addtionalPrice + 2.22;
             }
-            if (items[counter]==7) {
-                addtionalPrice=addtionalPrice+4.65;
+            if (items[counter] == 7) {
+                addtionalPrice = addtionalPrice + 4.65;
             }
-            if (items[counter]==8) {
-                addtionalPrice=addtionalPrice+5.00;
+            if (items[counter] == 8) {
+                addtionalPrice = addtionalPrice + 5.00;
             }
-         }
+        }
         NormalPizza.setPrice(addtionalPrice);
         HPizza.setPrice(addtionalPrice);
         PrePizza.setPrice(addtionalPrice);
+        for (int i = 0; i < items.length; i++) {
+            HPizza.setList(items[i], i);
+            NormalPizza.setList(items[i], i);
+            PrePizza.setList(items[i], i);
+
+        }
+
+
     }
-
-
 
 }
--- The program/src/Preminum_Pizza.java
+++ The program/src/Preminum_Pizza.java
@@ -1,31 +1,109 @@
 public class Preminum_Pizza {
-    private Integer breadType=0;
-    private Integer meat=0;
-    private Double price=0.0;
+    private Integer breadType = 0;
+    private Integer meat = 0;
+    private Double price = 0.0;
+    private Integer list[];
+    private Integer counter = 0;
 
-    public Preminum_Pizza(Integer bread, Integer meatType) {
+    public Preminum_Pizza(Integer bread, Integer meatType, Double currentPrice) {
         this.breadType = bread;
         this.meat = meatType;
+        this.price = currentPrice;
     }
 
     public void setBtype(Integer breadType) {
-        this.breadType=breadType;
+        this.breadType = breadType;
     }
+
     public void setMtype(Integer meat) {
-        this.meat=meat;
+        this.meat = meat;
     }
-    public void setPrice(Double addprice){
-        this.price=addprice;
+
+    public void setPrice(Double addprice) {
+        this.price = addprice;
     }
-    public Integer getBtype()
-    {
+
+    public void setList(Integer addOnItem, Integer position) {
+        this.list[position] = addOnItem;
+    }
+
+    public Integer getBtype() {
         return breadType;
     }
-    public Integer getMtype(){
+
+    public Integer getMtype() {
         return meat;
     }
-    public Double getPrice(){
+
+    public Double getPrice() {
         return price;
     }
 
+    public Integer getList(Integer position) {
+        return list[position];
+    }
+
+    public void viewPrice() {
+        Double totalPrice = 0.0;
+
+        System.out.println("This is your Bill till now");
+        if (breadType == 0) {
+            System.out.println("Whole wheat bread-----------$2.00");
+        } else {
+            System.out.println("Kurakhan bread-----------$3.00");
+            totalPrice = totalPrice + 3.00;
+        }
+        switch (meat) {
+            case 1:
+                System.out.println("Chicken-----------$5.00");
+                totalPrice = totalPrice + 5.00;
+                break;
+            case 2:
+                System.out.println("Beef-----------$6.00");
+                totalPrice = totalPrice + 6.00;
+                break;
+            case 3:
+                System.out.println("Mutton-----------$8.00");
+                totalPrice = totalPrice + 8.00;
+                break;
+            case 4:
+                System.out.println("Seafood-----------$6.50");
+                totalPrice = totalPrice + 6.50;
+                break;
+        }
+        while (counter < 6) {
+            Integer listItem = getList(counter);
+            switch (listItem) {
+                case 1:
+                    System.out.println("Cabbage-----------$ 2.89");
+                    break;
+                case 2:
+                    System.out.println("Carrots-----------$3.5");
+                    break;
+                case 3:
+                    System.out.println("Extra cheese-----------$5.00");
+                    break;
+                case 4:
+                    System.out.println("Extra sauce-----------$5.00");
+                    break;
+                case 5:
+                    System.out.println("Extra Meat-----------$5.00");
+                    break;
+                case 6:
+                    System.out.println("Spicy-----------$5.00");
+                    break;
+                case 7:
+                    System.out.println("Garlic Bread-----------$5.00");
+                    break;
+                case 8:
+                    System.out.println("Thin Crust-----------$5.00");
+                    break;
+            }
+            totalPrice = totalPrice + price;
+            System.out.println();
+            System.out.println(("The grand Total of your bill is......") + totalPrice);
+        }
+
+
+    }
 }
